612cb0d697a050d6781215fa93f8e92d49af8b19
jcollado7 <87857547+jcollado7@users.noreply.github.com>
2021-08-02 03:59:03 +0200

    Update log.py

--- log.py
+++ log.py
@@ -12,6 +12,7 @@ import assistant_functions as asf
 import os
 import logging.handlers as handlers
 import re
+import json
 
 
 class LogAgent(Agent):
@@ -37,13 +38,15 @@ class LogAgent(Agent):
                         logger.info(msg.body)
                         print("Coil status updated")
                     elif msg_sender_jid == "dynrct_r00":
+                        print("MSG", msg)
                         launcher_df = pd.read_json(msg.body)
                         asf.change_warehouse(launcher_df, my_dir)
                         coils = launcher_df.loc[0,'list_coils']
                         locations = launcher_df.loc[0, 'list_ware']
                         code = launcher_df.loc[0, 'order_code']
-                        n = f' Order Code: {code} with code coils: [{coils}] registered in locations [{locations}].'
-                        logger.info(n)
+                        order = f' Order Code: {code} with code coils: [{coils}] registered in locations [{locations}].'
+                        order = json.dumps(order)
+                        logger.info(order)
                     elif msg_sender_jid == "browser":
                         x = re.search("{", msg.body)
                         if x:
@@ -54,11 +57,18 @@ class LogAgent(Agent):
                                     msg_to_br = asf.msg_to_br(msg, my_dir)
                                     await self.send(msg_to_br)
                 else:
-                    logger.debug(f"Log_agent didn't receive any msg in the last {wait_msg_time}s") ####corregir, wait_msg_time es muy poco tiempo
+                    msg = f"Log_agent didn't receive any msg in the last {wait_msg_time}s"
+                    msg = json.dumps(msg)
+                    logger.debug(msg)
             elif log_status_var == "stand-by":
-                logger.debug(f"Log agent status: {log_status_var}")
+                status_msg = f"Log agent status: {log_status_var}"
+                status_msg = json.dumps(status_msg)
+                logger.debug(status_msg)
+
                 log_status_var = "on"
-                logger.debug(f"Log agent status: {log_status_var}")
+                on_msg = f"Log agent status: {log_status_var}"
+                on_msg = json.dumps(on_msg)
+                logger.debug(on_msg)
             else:
                 logger.debug(f"Log agent status: {log_status_var}")
                 log_status_var = "stand-by"
@@ -91,7 +101,7 @@ if __name__ == "__main__":
     delete_order = args.delete_order
     my_full_name = asf.my_full_name(my_name, args.agent_number)
     wait_msg_time = args.wait_msg_time
-    log_status_var = "Stand-by"
+    log_status_var = "stand-by"
 
 
     """Logger info"""
@@ -112,7 +122,10 @@ if __name__ == "__main__":
         logger.setLevel(logging.CRITICAL)
     else:
         print('not valid verbosity')
-    logger.debug(f"{my_name}_agent started")
+
+    start_msg = f"{my_name}_agent started"
+    start_msg = json.dumps(start_msg)
+    logger.debug(start_msg)
 
     """XMPP info"""
     log_jid = asf.agent_jid(my_dir, my_full_name)
@@ -128,5 +141,7 @@ if __name__ == "__main__":
     else:
         log_agent.stop()
         log_status_var = "off"
-        logger.critical(f"{my_full_name}_agent stopped, coil_status_var: {log_status_var}")
+        stop_msg_log = f"{my_full_name}_agent stopped, coil_status_var: {log_status_var}"
+        stop_msg_log = json.dumps(stop_msg_log)
+        logger.critical(stop_msg_log)
         quit_spade()
